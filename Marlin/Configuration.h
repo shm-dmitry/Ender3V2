174c6f0a32741838368a4707ec0d348bd6a13038
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2024-04-03 08:20:04 -0500

    Fix mistype update Paypal FUNDING.yml

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1353,7 +1353,7 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN  // Probe connected to BLTouch port
+#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN  // Probe connected to BLTouch port
 
 // Force the use of the probe for Z-axis homing
 #define USE_PROBE_FOR_Z_HOMING
